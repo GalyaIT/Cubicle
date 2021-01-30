7a8e18dec5214c63cab53921548b7fd03beb9a31
GalyaIT <givanova76@gmail.com>
2021-01-30 11:44:17 +0200

    Add page title to layouts

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,10 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/',(req,res)=>{   
-    res.render('home')
+    res.render('home',{title:'Cubicle'})
 });
 router.get('/create',(req,res)=>{   
-    res.render('create')
+    res.render('create', {title:'Create Cube Page'})
 });
 
 module.exports=router;

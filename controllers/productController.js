3d138d83a05cf6afbd71262a04fada06cd900e48
GalyaIT <givanova76@gmail.com>
2021-01-30 13:32:55 +0200

    Save data to json file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,27 @@
-const {Router} = require('express');
+const { Router } = require('express');
 
+const productService = require('../services/productService');
 const router = Router();
 
-router.get('/',(req,res)=>{   
-    res.render('home',{title:'Cubicle'})
+router.get('/', (req, res) => {
+    res.render('home', { title: 'Cubicle' })
 });
-router.get('/create',(req,res)=>{   
-    res.render('create', {title:'Create Cube Page'})
+router.get('/create', (req, res) => {
+    res.render('create', { title: 'Create Cube Page' })
 });
 
-router.post('/create', (req, res)=>{
+router.post('/create', (req, res) => {
     console.log(req.body);
-    res.send('created')
+    // validate input
+  
+    productService.create(req.body);
+
+  
+    res.redirect('/products')
 })
 
-router.get('/details/:productId',(req,res)=>{   
-    res.render('details', {title:'Product details'})
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Product details' })
 });
 
-module.exports=router;
+module.exports = router;

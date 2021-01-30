0dc4abe6c5333f42db7cc71cdd5cb6e2853831d7
GalyaIT <givanova76@gmail.com>
2021-01-30 13:36:49 +0200

    Save data to json file

---
+++ services/productService.js
@@ -0,0 +1,30 @@
+const uniqId = require('uniqId');
+const Cube = require('../models/Cube');
+const fs = require('fs');
+let productsData = require('../config/products.json');
+
+function getAll(){
+    return productsData;
+}
+
+function create(data) {
+    let cube = new Cube(
+        uniqId(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel
+    );
+console.log(cube);
+    productsData.push(cube);
+
+    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+    });
+}
+module.exports = {
+    create
+}

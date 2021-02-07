9e66bff9a8123b2eb363ec8f864176c16c4b8b37
GalyaIT <givanova76@gmail.com>
2021-02-07 18:11:44 +0200

    add login and register

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const config=require('./config/config');
+const config = require('./config');
 const routes = require('./routes');
 
 const app = express();
@@ -15,4 +15,4 @@ app.use(routes);
 
 
 
-app.listen(config.PORT, ()=>console.log(`Server is running on port ${config.PORT}....`));
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}....`));
--- models/Accessory.js
+++ models/Accessory.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const accessorySchema = new mongoose.Schema({
+    id:mongoose.Types.ObjectId,
     name: {
         type: String,
         required: true,
